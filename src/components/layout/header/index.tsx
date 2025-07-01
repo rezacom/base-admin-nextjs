8914c3b32bf9190bd838fcb5349bf97923822a25
kamil <reza.shoja@utechia.com>
2025-07-01 10:32:17 +0330

    -

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,17 +1,20 @@
+"use client";
 import ThemeSwitcher from "@/components/themeSwitcher";
 import Image from "next/image";
 import React from "react";
 
 function Header() {
+  const toggleSidebar = () => {
+    document.getElementById("sidebarDashboard")?.classList.toggle("-translate-x-full");
+  };
+
   return (
     <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-start rtl:justify-end">
             <button
-              data-drawer-target="sidebarDashboard"
-              data-drawer-toggle="sidebarDashboard"
-              aria-controls="sidebarDashboard"
+              onClick={() => toggleSidebar()}
               type="button"
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             >
